1d9d6456f4e268e94ac74322bdb32e0c5df72ff0
Madfinn <madfinn@outlook.com>
2017-12-15 18:50:03 +0200

    Tanatos lp (5 sections)

--- HomeWork/HW23/js/script.js
+++ HomeWork/HW23/js/script.js
@@ -16,60 +16,88 @@ window.onload = function(){
 Выделить нужно следующими стилями:
 outline: 3px solid red; background-color: lightblue;
 */	
-		var wrapper = document.querySelectorAll('.page-wrapper');
-		var ZenIdElements = document.querySelectorAll('[id^="zen"]');
-		var header = document.querySelectorAll('header');
-		var h1 = document.querySelectorAll('header h1');
-		var h2 = document.querySelectorAll('header>h2');
-		var h3 = document.querySelectorAll('[id^="zen"]>h3');
-		var firstP = document.querySelectorAll('[id^="zen"]>p');
-		var abbr = document.querySelectorAll('abbr');
-		var links = document.querySelectorAll('a');
-		var footerLinks = document.querySelectorAll('footer a');
-		var aside = document.querySelectorAll('.sidebar');
-		var designClassElements = document.querySelectorAll('div[class^="design"]');
-		var asideH3 = document.querySelectorAll('div[class^="design"]+h3');
-		var asideNav = document.querySelectorAll('div[class^="design"]>nav');
-		var asideLists = document.querySelectorAll('.sidebar li');
+		var findButton = document.querySelector('.selector-find');
+		var selector = document.querySelector('.selector');
+		var next = document.querySelector('.selector-next');
+		var prev = document.querySelector('.selector-prev');
+		var parent = document.querySelector('.nav-top');
+		var firstChildEl = document.querySelector('.nav-bottom');
+		var nextSibling = document.querySelector('.nav-right');
+		var prevSibling = document.querySelector('.nav-left');
 
-		function findElement (element){
-			for (var i = 0; i < element.length; i++) {
-				element[0].style.backgroundColor = 'lightblue';
-				element[0].style.outline = '3px solid red';
-			}	
+		var allElements = [];
+		var counter = 0;
+		var currentElement;
+		var parentElement;
+
+		findButton.onclick = function() {
+			var selectorQuery = selector.value.trim(); // trim - обрезает пробелы по бокам (слева и справа)
+			if (!selectorQuery === '')
+				return;
+
+			allElements = document.querySelectorAll(selectorQuery);
+			if (allElements.length) {
+				currentElement = allElements[counter];
+				currentElement.parentElement = allElements[counter];
+				selectElement();
+				next.disabled = !allElements[counter + 1];
+			} else {
+				alert('There is no elements by selector ' + selectorQuery);
+			}
+		};
+
+		function selectElement() {
+			currentElement.style.backgroundColor = 'lightblue';
+			currentElement.style.outline = '3px solid red';
 		}
-		
-		findElement(wrapper);
-		findElement(ZenIdElements);
-		findElement(header);
-		findElement(h1);
-		findElement(h2);
-		findElement(h3);
-		findElement(firstP);
-		findElement(abbr);
-		findElement(links);
-		findElement(footerLinks);
-		findElement(aside);
-		findElement(designClassElements);
-		findElement(asideNav);
-		findElement(asideH3);
-		findElement(asideLists);
 
-		/*2. Сделать возможным переход по найденным в предыдущем задании элементам, 
-		используя кнопки Next и Previous. При переходе на следующий элемент - снимаем 
-		выделение с предыдущего и ставим на тот, на который уже перешли. Если нет элементов 
-		впереди - кнопка Next должна быть неактивна, если сзади, то Previous также должна
-		иметь атрибут disabled.*/
+		function unSelectElement() {
+			currentElement.removeAttribute('style');
+		}
 
-/*		var inputSelector = document.getElementsByClassName('selector').value;
-		var findButton = document.getElementsByClassName('selector-find');
-		findElement(document.querySelectorAll(findButton));
-  		findButton.onclick = function
-  	NextButton.onclick = function(click, findElement(document.querySelectorAll(findButton)) ) {
-    	
-  };*/
+		next.addEventListener('click', function(){
+			unSelectElement();
+
+			if (allElements[counter + 1]) {
+				counter++;
+				currentElement = allElements[counter];
+				selectElement();
+
+				next.disabled = !allElements[counter + 1];
+				prev.disabled = !allElements[counter - 1];
+			}
+		});
+
+		prev.addEventListener('click', function(){
+			unSelectElement();
+
+			if (allElements[counter - 1]) {
+				counter--;
+				currentElement = allElements[counter];
+				selectElement();
+
+				next.disabled = !allElements[counter + 1];
+				prev.disabled = !allElements[counter - 1];
+			}
+		});
+
+		parent.addEventListener('click', function(){
+			unSelectElement();
+			next.disabled;
+			prev.disabled;
+
+			if (allElements[counter - 1]) {
+				counter--;
+				currentElement = currentElement.parentElement;
+				selectElement();
+				
+
+				if (currentElement) {	
+				parent.disabled = !currentElement.parentElement;
+				}
+				
+			}
+		});
 
 	
-	
-	// Enter your code here 
-};
+	};	
