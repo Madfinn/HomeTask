e34f3608e56070d944dfd4c67678fe5b3d918ede
Madfinn <madfinn@outlook.com>
2018-01-13 00:27:26 +0200

    fix stars

--- HomeWork/HW28/collaboration/public/js/scripts.js
+++ HomeWork/HW28/collaboration/public/js/scripts.js
@@ -6,25 +6,23 @@ var url = '/collaboration';
 //1.
 table.addEventListener('click', function(event){
 var req = new XMLHttpRequest();
-var n = event.target.getAttribute('id');
+var n = event.target.getAttribute('id').substring(5);
 var s = event.target.getAttribute('src') === "star_off.gif" ? 1 : 0;
-var params = '/collabollaboration?n=99&s=1';
-// "n=" + n + "&s=" + s;
-req.open('GET', url);
-
-
+var params =  "n=" + n + "&s=" + s;
+req.open('GET', url+"?"+params, true);
   req.onreadystatechange = function(){
 	if (req.readyState == 4 && req.status == 200) {
   		fireStar(req.responseText);
   	}	
   }
-  req.send(params);
+  req.send(null);
 })
 
 function fireStar(starStatus) {
 	var arr = starStatus.split('');
 	for (var i = 0; i < 100; i++) {
-		stars[i].src = arr[i] === '0' ? "star_on.gif" : "star_off.gif";
+		stars[i].src = arr[i] === '0' ? "star_off.gif" : "star_on.gif";
+		console.log(starStatus);
 	}
 };
 
